b0be25a7fb3570ea993ab38c67367561678f12ee
Maksym Koitsan <maksym.k@flooow.agency>
2023-07-11 17:28:50 +0300

    fix stiky menu

--- js/template-parts/header/header.js
+++ js/template-parts/header/header.js
@@ -1,20 +1,19 @@
-import $  from 'jquery';
+import $ from "jquery";
 
-function header(){
-   $(document).ready(function(){
+function header() {
+  $(document).ready(function () {
     var w = $(window).width();
-    if(w < 1025){
-        var height = $('.hero').outerHeight();
-        $('.header__mobile').css('top', height);
+    if (w < 1025) {
+      var height = $(".hero").outerHeight();
+      // $('.header__mobile').css('top', height);
     }
-    $('.header__burger').click(function(){
-        $('.header__mobileMenu').addClass('opened');
+    $(".header__burger").click(function () {
+      $(".header__mobileMenu").addClass("opened");
     });
-    $('.header__burgerClose').click(function(){
-        $('.header__mobileMenu').removeClass('opened');
+    $(".header__burgerClose").click(function () {
+      $(".header__mobileMenu").removeClass("opened");
     });
-   });
+  });
 }
-
 
 export { header };
