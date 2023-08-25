7d2285a9f236d92f60971ece09194977c20fc33a
Maksym Koitsan <maksym.k@flooow.agency>
2023-08-25 11:38:32 +0300

    fix bg

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 	</div>
 	<div class="footer__bg">
-		<img src="<?php echo get_template_directory_uri(); ?>/assets/images/footer_bg.svg" alt="">
+		<!-- <img src="<?php echo get_template_directory_uri(); ?>/assets/images/footer_bg.svg" alt=""> -->
 	</div>
 	<?php get_template_part('template-parts/footer/footer'); ?>
 
