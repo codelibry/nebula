a8984779182a30bcdb81206a77843a5c86439479
Vitalii Omelchenko <67092563+vitaliiomelchenko@users.noreply.github.com>
2023-09-08 15:03:49 +0300

    404 page development

--- 404.php
+++ 404.php
@@ -1,7 +1,13 @@
 <?php get_header(); ?>
 
-<div class="page-blocks">
-        <h1>404</h1>
-</div>
+<section class="errorBlock">
+        <div class="container">
+                <div class="errorBlock__content">
+                        <h1 class="errorBlock__title"><?php _e('404','wsig');?></h1>
+                        <h4 class="errorBlock__subtitle"><?php _e("Ooops! <br>This page wasn't found.","nebula");?></h4>
+                        <div class="errorBlock__button"><a href="<?php echo get_home_url(  ) ?>" class="btn btn--blue btn--small font--weight--700">Go Home</a></div>
+                </div>
+        </div>
+</section>
 
 <?php get_footer(); ?>
